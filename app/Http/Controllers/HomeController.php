3e0762e825a2ae4d42c28d927c021a3201cae920
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-05-28 23:44:00 +0700

    update route, buat request, buat controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Requests\StoreUser;
+
+class HomeController extends Controller
+{
+    public function create()
+    {
+        return view('createUser');
+    }
+
+    public function store(StoreUser $request)
+    {
+        $input = $request->all();
+        $user = User::create($input);
+
+        return back()->with('success', 'User created successfully');
+    }
+}
